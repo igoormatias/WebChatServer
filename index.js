f1995ef58b3edb977e98781b3a1c645c40f1bd70
Igor Matias <igorhmatias@gmail.com>
2020-01-20 21:09:27 -0300

    Finish backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const socketio = require ('socket.io')
 const http = require ('http')
 
-const { addUser,removeUser,getUser,getUsesInRoom } = require('./users.js')
+const { addUser,removeUser,getUser,getUsersInRoom } = require('./users.js')
 
 const PORT = process.env.PORT || 5000
 
@@ -12,30 +12,43 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+
+// Logar na sala
 io.on('connection', (socket) => {
  socket.on('join', ({name, room}, callback)=>{
         const { error, user } = addUser({id: socket.id, name, room});
 
         if (error) return callback(error)
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, Bem vindo a sala ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, Entrou no grupo!`})
+        socket.emit('message', { user: 'admin', text: `${user.name.charAt(0).toUpperCase() + user.name.slice(1)}, Bem vindo a sala ${user.room.charAt(0).toUpperCase() + user.room.slice(1)}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name.charAt(0).toUpperCase() + user.name.slice(1)}, Entrou no grupo!`})
 
         socket.join(user.room)
+
+        io.to(user.room).emit('roomData',{room: user.room, users: getUsersInRoom(user.room)})
 
         callback();
     });
 
+
+    // Enviar mensagem
     socket.on('sendMessage', (message, callback)=>{
         const user = getUser(socket.id)
+        
 
-        io.to(user.room).emit('message', { user: user.name, text: message})
+        io.to(user.room).emit('message', { user: user.name.charAt(0).toUpperCase() + user.name.slice(1), text: message})
+        io.to(user.room).emit('roomData', { room: user.room,  users: getUsersInRoom(user.room)})
 
         callback();
     })
-    socket.on('disconnect', () => {
-        console.log('Usuário acabou de sair')
 
+    // Sair da sala
+    socket.on('disconnect', () => {
+       const user = removeUser(socket.id)
+       
+       if (user) {
+           io.to(user.room).emit('message', { user: 'admin', text: `${user.name.charAt(0).toUpperCase + user.name.slice(1)} acabou de sair`})
+       }
     })
 });
 
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const router = express.Router();
 
 router.get('/', (req,res) =>{
-    res.send('ser is running')
+    res.send('server is running')
 });
 
 module.exports = router;
